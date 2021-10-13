a53fc11d49fe6b1b647130846cdeef7c6fba028e
souptik4572 <souptiksarkar4572@gmail.com>
2021-10-13 06:50:18 +0530

    added sql lite url as well

--- base.py
+++ base.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(
-    'postgresql://glgxilut:u12-UVGL0349qBQg8rSN-75E5bohRt3P@surus.db.elephantsql.com/glgxilut')
+engine = create_engine('postgresql://glgxilut:u12-UVGL0349qBQg8rSN-75E5bohRt3P@surus.db.elephantsql.com/glgxilut')
+# engine = create_engine('sqlite:///celebs.db')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
